ae8d2b95145037c1db5962644f0f78eadc360487
sen2y <pse6704@naver.com>
2023-06-20 04:47:06 +0900

    18. @NamedQuery 사용하기

--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -2,6 +2,7 @@ package com.example.demo.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.NamedQuery;
 import javax.persistence.Table;
 import org.hibernate.annotations.GenericGenerator;
 import lombok.AllArgsConstructor;
@@ -15,12 +16,14 @@ import lombok.NoArgsConstructor;
 @Data
 @Entity
 @Table(name ="Todo")
-public class TodoEntity {
+	@NamedQuery(name="TodoRepository.searchByUserId",
+	query ="select t from TodoEntity t where t.userId = ?1")
+	public class TodoEntity {
 	@Id
 	@GeneratedValue(generator="system-uuid") // id 자동으로 성성
 	@GenericGenerator(name="system-uuid",strategy="uuid")
 	private String id;
-	private String Userid;
+	private String userId;
 	private String title;
 	private boolean done;
-}
+	}
